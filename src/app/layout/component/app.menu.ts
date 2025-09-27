4c5300b942c97f1676295070154c145726a3633b
vitoncon <nguyentienvietk9@gmail.com>
2025-09-27 22:48:06 +0700

    fix: ra dashboard

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -22,7 +22,7 @@ export class AppMenu {
         this.model = [
             {
                 label: 'Home',
-                items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
+                items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/dashboard'] }]
             },
             {
                 label: 'KHÓA HỌC & LỚP HỌC',
