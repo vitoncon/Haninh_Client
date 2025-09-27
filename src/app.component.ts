6f507abe3a1e128c721d710735bd886855549c43
vitoncon <nguyentienvietk9@gmail.com>
2025-09-27 22:44:18 +0700

    add:toast notification

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    imports: [RouterModule,ToastModule],
+    template: `
+        <router-outlet></router-outlet>
+        <p-toast></p-toast>`
 })
 export class AppComponent {}
