47669b0faf70471ea9d0e73c493f1494d18def9b
vitoncon <nguyentienvietk9@gmail.com>
2025-09-27 22:47:22 +0700

    add: gán quyền

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { roleGuard } from '../core/guards/role.guard';
 import { Courses } from './courses/components/courses';
 import { Class } from './class-management/components/class/class';
 import { Schedule } from './schedule/components/schedule/schedule';
@@ -12,18 +13,18 @@ import { Fees } from './fees/components/fees/fees';
 
 export default [   
     // quan ly khoa hoc
-    { path: 'courses', data: { breadcrumb: 'Quản lý khóa học' }, component: Courses},
-    { path: 'class', data: { breadcrumb: 'Quản lý lớp học' }, component: Class },
-    { path: 'schedule', data: { breadcrumb: 'Lịch học' }, component: Schedule },
+    { path: 'courses', data: { breadcrumb: 'Quản lý khóa học', roles: [1] }, canActivate: [roleGuard], component: Courses},
+    { path: 'class', data: { breadcrumb: 'Quản lý lớp học', roles: [1] }, canActivate: [roleGuard], component: Class },
+    { path: 'schedule', data: { breadcrumb: 'Lịch học', roles: [1,2,3] }, canActivate: [roleGuard], component: Schedule },
     // // quan ly hoc vien
-    { path: 'students', data: { breadcrumb: 'Quản lý học viên' }, component: Students },
-    { path: 'study-results', data: { breadcrumb: 'Kết quả học tập' }, component: StudyResults },
-    { path: 'certificates', data: { breadcrumb: 'Chứng chỉ' }, component: Certificates },
+    { path: 'students', data: { breadcrumb: 'Quản lý học viên', roles: [1,2] }, canActivate: [roleGuard], component: Students },
+    { path: 'study-results', data: { breadcrumb: 'Kết quả học tập', roles: [1,2,3] }, canActivate: [roleGuard], component: StudyResults },
+    { path: 'certificates', data: { breadcrumb: 'Chứng chỉ', roles: [1] }, canActivate: [roleGuard], component: Certificates },
     // // quan ly giang vien
-    { path: 'teacher', data: { breadcrumb: 'Quản lý giảng viên' }, component: Teacher },
-    { path: 'teaching-assignments', data: { breadcrumb: 'Phân công giảng dạy' }, component: TeachingAssignments },
+    { path: 'teacher', data: { breadcrumb: 'Quản lý giảng viên', roles: [1] }, canActivate: [roleGuard], component: Teacher },
+    { path: 'teaching-assignments', data: { breadcrumb: 'Phân công giảng dạy', roles: [1] }, canActivate: [roleGuard], component: TeachingAssignments },
     // // quan ly tai chinh
-    { path: 'fees', data: { breadcrumb: 'Quản lý học phí' }, component: Fees },
+    { path: 'fees', data: { breadcrumb: 'Quản lý học phí', roles: [1] }, canActivate: [roleGuard], component: Fees },
     // default route
     { path: '**', redirectTo: '/notfound' },
 ] as Routes;
