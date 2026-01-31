f645ba46bc4b0d63b052b389e30231072b27564b
Việt <nguyentienvietk9@gmail.com>
2026-01-31 20:31:01 +0700

    test

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,10 +1,5 @@
-import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-    const authService = inject(AuthService);
-
-    // MOCK: luôn coi là đã đăng nhập
-    return true;
+export const authGuard: CanActivateFn = () => {
+    return true; // LUÔN CHO PHÉP TRUY CẬP
 };
