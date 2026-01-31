439607ab12543ded44def8a8f71172998016caea
Việt <nguyentienvietk9@gmail.com>
2026-01-31 20:31:27 +0700

    test

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,10 +1,5 @@
-import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { CanActivateFn } from '@angular/router';
 
-export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
-    const authService = inject(AuthService);
-
-    // MOCK: luôn coi là đúng role (admin)
-    return true;
+export const roleGuard: CanActivateFn = () => {
+    return true; // LUÔN CHO PHÉP
 };
