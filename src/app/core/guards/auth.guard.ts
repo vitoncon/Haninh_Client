8ccd64c59704831c2b41f50934168d52be1634ba
Việt <nguyentienvietk9@gmail.com>
2026-01-31 20:23:58 +0700

    test

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,16 +1,10 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
-    const router = inject(Router);
 
-    if (authService.isAuthenticated()) {
-        return true;
-    }
-
-    return router.createUrlTree(['/auth/login'], { queryParams: { redirected: 'true' } });
+    // MOCK: luôn coi là đã đăng nhập
+    return true;
 };
-
-
